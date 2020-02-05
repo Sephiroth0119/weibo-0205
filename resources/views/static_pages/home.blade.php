ebc8a90f5b9e336d3049f97b28c8de141b85adb9
vagrant <vagrant@homestead>
2020-02-05 12:25:48 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+    <div class="jumbotron">
+      <h1 >Hello Laravel</h1>
+      <p class="lead">你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。</p>
+      <p>一切又要重新开始</p>
+      <p>
+        <a class="btn btn-success btn-lg" href="J#" role="button">现在开始注册</a>
+      </p>
+    </div>
 @endsection
