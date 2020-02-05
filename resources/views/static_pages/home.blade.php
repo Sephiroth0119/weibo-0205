b75afb7c559200c800cceb470424fb7f6b7fa8d2
vagrant <vagrant@homestead>
2020-02-05 13:03:28 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
       <p class="lead">你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。</p>
       <p>一切又要重新开始</p>
       <p>
-        <a class="btn btn-success btn-lg" href="J#" role="button">现在开始注册</a>
+      <a class="btn btn-success btn-lg" href="{{ route('signup') }}" role="button">现在注册</a>
       </p>
     </div>
-@endsection
+@stop
