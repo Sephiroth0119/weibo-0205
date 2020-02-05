f3f5b53dfe3637f52c38ebc4a0c146600dc2d53c
vagrant <vagrant@homestead>
2020-02-05 16:42:05 +0000

    完成用户注册功能

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -27,8 +27,8 @@
       </div>
 
       <div class="form-group">
-        <label for="passoword_confirmation">确认密码</label>
-        <input type="password" name="passoword_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        <label for="password_confirmation">确认密码</label>
+        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
       </div>
 
       <button type="submit" class="btn btn-primary">注册</button>
