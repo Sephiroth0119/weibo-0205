cc9693f0e79cfd7eab11ad8dfcfaf1c63c8b66d2
vagrant <vagrant@homestead>
2020-02-08 15:03:36 +0000

    ‘关注用户动态流’

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -60,7 +61,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
@@ -76,7 +80,7 @@ class User extends Authenticatable
 
     public function follow($user_ids)
     {
-        if ( ! is_array($user_ids)) {
+        if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->sync($user_ids, false);
@@ -84,7 +88,7 @@ class User extends Authenticatable
 
     public function unfollow($user_ids)
     {
-        if ( ! is_array($user_ids)) {
+        if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->detach($user_ids);
